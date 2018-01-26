1283dc5d9bdfca8833737c6de6df02147cd45e73
Daniel Kalevski <kalevski.daniel@gecko.mk>
2018-01-26 16:19:27 +0100

    removed one row from the grid

--- src/gameObject/worldField.js
+++ src/gameObject/worldField.js
@@ -10,7 +10,7 @@ class WorldField extends Phaser.Group {
     constructor(game, eventHandler) {
         super(game, game.world, 'worldField');
         this.background = this.create(0, 0, 'ui-world-' + eventHandler.world['type']);
-        this.grid = this.create(0, 100, 'ui-arena-grid');
+        this.grid = this.create(0, 110, 'ui-arena-grid');
         
         eventHandler.world.userList.forEach((nickname, index) => {
             let creatureData = eventHandler.world.user[nickname].data.creature;
